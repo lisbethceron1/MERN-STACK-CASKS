662ceaee12c952669e5e2f46d8cb6a572f345828
lisbethceron1 <flceron0@misena.edu.co>
2023-09-06 18:15:57 -0500

    ejercicio mern

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+const express=require('express')
+const morgan=require('morgan')
+const path = require('path')
+const {mongoose} = require('./database')
+const app = express();
+//settings configu
+app.set('port', process.env.PORT || 3000)
+
+//middleweares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//routes
+app.use('/api/tasks',require('./routes/task.routes'));
+//static file
+
+app.use(express.static(path.join(__dirname, 'public')))
+//starting the server
+app.listen(app.get('port'),()=>{
+    console.log(`Server on port ${app.get('port')}`)
+});
