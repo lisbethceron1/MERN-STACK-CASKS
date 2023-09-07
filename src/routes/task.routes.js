4f42febff60d8673594338aedcfa52b59a9c8034
lisbethceron1 <flceron0@misena.edu.co>
2023-09-07 15:44:54 -0500

    actualizacion version 1

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const router = express.Router()
 
-const Task = require('../models/task')
+const Task = require('../models/task');
+const task = require('../models/task');
 router.get('/', async (req, res) => {
     const tasks = await Task.find();
    
     res.json(tasks);
 
 });
+router.get('/:id', async(req, res)=>{
+  const task = await  Task.findById(req.params.id)
+  res.json(task);
+})
 router.post('/', async(req, res)=>{
-    console.log(req.body)
+   const {title, description} = req.body;
+   const task = new Task ({title, description })
+ await task.save()
+    res.json({status:'task saved'})
+})
+router.put('/:id' , async(req, res)=>{
+  const {title,description} = req.body
+  const newTASK = { title,description};
+   await task.findByIdAndUpdate(req.params.id, newTASK)
+   
+    res.json({status:'task update'})
+})
+router.delete('/:id',async(req, res)=>{
+   await Task.findByIdAndRemove(req.params.id)
+   res.json({status:'task deleted'})
 })
 module.exports = router;
